2756d09f481fe6ed4c628a6a7cb25dc1899511c8
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-09-22 01:20:49 -0300

    Projeto de grador de senhas

---
+++ src/pages/passwords/index.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export function Passwords() {
+    return(
+        <SafeAreaView style={{ flex: 1}}>
+          <View style={styles.header}>
+            <Text style={styles.title}>Minhas senhas</Text>
+          </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    title: {
+        color: '#000FFF'
+    }
+})
